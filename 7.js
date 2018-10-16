30aac59bad7ba11639cade6dd7bea3fffdb89f7f
Victor Borshchov <victor.borshchov@gmail.com>
2018-10-16 23:55:04 +0300

    improve function for Queen task

--- 7.js
+++ 7.js
@@ -1,15 +1,23 @@
 'use strict'
+var bench = require('./bench')
 
 var getQueenPossiblePoints = function(point) {
     var result = [];
-    for (var i = 1;  i <= 8; i++) {
-        for (var j = 1; j <=8 ;j++) {
-            if((i !== point.x || j !== point.y) && (( i === point.x || j === point.y) || (Math.abs(i-point.x) === Math.abs(j-point.y)))) {
-                result.push({x: i, y: j})
-            }
+
+    for (var i = 1; i <= 8; i++) {
+        if (i !== point.x && i !== point.y) {
+            result.push({x: point.x, y: i})
+            result.push({x: i, y: point.y})
+            if (point.x - i > 0 && point.y - i > 0) { result.push({x: point.x - i, y: point.y - i});}
+            if (point.x - i > 0 && point.y + i < 9) { result.push({x: point.x - i, y: point.y + i});}
+            if (point.x + i < 9 && point.y - i > 0) { result.push({x: point.x + i, y: point.y - i});}
+            if (point.x + i < 9 && point.y + i < 9) { result.push({x: point.x + i, y: point.y + i});}
         }
     }
     return result;
 }
 
-console.log(getQueenPossiblePoints({x: 1, y: 1}))
+var times = 1000000
+var point = {x:5, y:6}
+
+bench(getQueenPossiblePoints, times, [point], this)
